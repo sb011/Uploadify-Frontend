cbb79d4a39e69468180b363b332aa220468a1300
sb011 <smitbhoraniya11@gmail.com>
2023-11-01 11:50:12 +0530

    Deploy changes

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: "https://uploadify-backend.onrender.com/api/:path*",
       },
     ];
   },
